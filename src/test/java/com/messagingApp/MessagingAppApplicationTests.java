1dd7426f07a4b3230d9e694bb39d7befc094a938
LENOVO\User <sharmaavnish@icloud.com>
2023-05-27 21:01:06 -0400

    Completed

---
+++ src/test/java/com/messagingApp/MessagingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.messagingApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessagingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
